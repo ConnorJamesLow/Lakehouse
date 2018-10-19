c7dcf6e8348d616ee70939681bc8cff095fe5337
Connor Low <clow2@my.gcu.edu>
2018-10-19 11:51:38 -0700

    Started architecture

---
+++ Lakehouse/Models/LocationDescription.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lakehouse.Models
+{
+    public class LocationDescription
+    {
+        public int LocationDescriptionId { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public ContentType Type { get; set; }
+    }
+}
---
+++ Lakehouse/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lakehouse.Models
+{
+    public class User
+    {
+
+        public int UserId { get; set; } = -1;
+        public string Name { get; set; } = "";
+        public string Phone { get; set; } = "";
+        public string Email { get; set; } = "";
+        public Role UserRole { get; set; } = Role.Unconfirmed;
+
+    }
+}
