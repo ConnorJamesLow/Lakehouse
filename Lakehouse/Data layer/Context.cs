06c20cd2dec2220b7a5ff3df2621c819ac23673f
Jonathan <jcouture@my.gcu.edu>
2018-10-20 09:54:23 -0700

    Database almost complete

---
+++ Lakehouse/Data layer/Context.cs
@@ -0,0 +1,14 @@
+﻿using Lakehouse.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lakehouse.Data_layer
+{
+    public class Context : DbContext
+    {
+        public DbSet<User> User { get; set; }
+    }
+}
---
+++ Lakehouse/Data layer/UserCrud.cs
@@ -0,0 +1,31 @@
+﻿using Lakehouse.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lakehouse.Data_layer
+{
+    public class UserCrud
+    {
+        private Context _context { get; set; }
+        public UserCrud(Context context)
+        {
+            _context = context;
+        }
+        public User Get(int ID)
+        {
+            return _context.User.FirstOrDefault(e => e.Id == ID);
+        }
+        public IEnumerable<User> GetAll()
+        {
+            return _context.User.ToList<User>();
+        }
+        public void Add(User user)
+        {
+            _context.Add(user);
+            _context.SaveChanges();
+        }
+
+    }
+}
